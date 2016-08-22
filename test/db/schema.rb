67e9ce4118e71031b5d0874c176645760def3b0d
Valdis Pornieks <valdis@ithouse.lv>
2016-08-22 12:07:26 +0300

    updated to work with ruby 2.3.1 and devise 4.2

--- test/db/schema.rb
+++ test/db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20111013050837) do
 
-  create_table "members", force: true do |t|
+  create_table "members", force: :cascade do |t|
     t.integer  "tenant_id"
     t.integer  "user_id"
     t.string   "first_name"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20111013050837) do
   add_index "members", ["tenant_id"], name: "index_members_on_tenant_id"
   add_index "members", ["user_id"], name: "index_members_on_user_id"
 
-  create_table "posts", force: true do |t|
+  create_table "posts", force: :cascade do |t|
     t.integer  "tenant_id"
     t.integer  "member_id"
     t.integer  "zine_id"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 20111013050837) do
   add_index "posts", ["tenant_id"], name: "index_posts_on_tenant_id"
   add_index "posts", ["zine_id"], name: "index_posts_on_zine_id"
 
-  create_table "sessions", force: true do |t|
+  create_table "sessions", force: :cascade do |t|
     t.string   "session_id", null: false
     t.text     "data"
     t.datetime "created_at"
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 20111013050837) do
   add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true
   add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at"
 
-  create_table "team_assets", force: true do |t|
+  create_table "team_assets", force: :cascade do |t|
     t.integer  "tenant_id"
     t.integer  "member_id"
     t.integer  "team_id"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 20111013050837) do
   add_index "team_assets", ["team_id"], name: "index_team_assets_on_team_id"
   add_index "team_assets", ["tenant_id"], name: "index_team_assets_on_tenant_id"
 
-  create_table "teams", force: true do |t|
+  create_table "teams", force: :cascade do |t|
     t.integer  "tenant_id"
     t.string   "name"
     t.datetime "created_at"
@@ -69,7 +69,7 @@ ActiveRecord::Schema.define(version: 20111013050837) do
 
   add_index "teams", ["tenant_id"], name: "index_teams_on_tenant_id"
 
-  create_table "tenants", force: true do |t|
+  create_table "tenants", force: :cascade do |t|
     t.integer  "tenant_id"
     t.string   "name"
     t.datetime "created_at"
@@ -79,14 +79,14 @@ ActiveRecord::Schema.define(version: 20111013050837) do
   add_index "tenants", ["name"], name: "index_tenants_on_name"
   add_index "tenants", ["tenant_id"], name: "index_tenants_on_tenant_id"
 
-  create_table "tenants_users", id: false, force: true do |t|
+  create_table "tenants_users", id: false, force: :cascade do |t|
     t.integer "tenant_id", null: false
     t.integer "user_id",   null: false
   end
 
   add_index "tenants_users", ["tenant_id", "user_id"], name: "index_tenants_users_on_tenant_id_and_user_id"
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                        default: "",    null: false
     t.string   "encrypted_password",           default: "",    null: false
     t.string   "reset_password_token"
@@ -113,7 +113,7 @@ ActiveRecord::Schema.define(version: 20111013050837) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
-  create_table "zines", force: true do |t|
+  create_table "zines", force: :cascade do |t|
     t.integer  "tenant_id"
     t.integer  "team_id"
     t.datetime "created_at"
